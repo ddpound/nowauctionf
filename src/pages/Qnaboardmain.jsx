92fa7d5133d1f58f8c4ec43902982f261bb72ae9
dpound <dpound@naver.com>
2022-04-27 01:31:35 +0900

    query string 라이브러리를 통해서 편하게 쿼리스트링을 다룰 수 있게 코드를 짜는중입니다.

--- src/pages/Qnaboardmain.jsx
+++ src/pages/Qnaboardmain.jsx
@@ -1,19 +1,27 @@
+import queryString from 'query-string'
+
+
 export default function Qnaboardmain(props){
-
-
     const searchParams = props.location.search
-
-    console.log(searchParams)
-
-    const obj = new URLSearchParams(searchParams)
-
+    //console.log(searchParams)
+    // 이 방법도 있다.
+    //const obj = new URLSearchParams(searchParams)
     // 반드시 key 값을 알아야 value에 접근할 수 있습니다.
-    
-    console.log(obj.get('startpage'))
+    // 그리고 브라우저 별로 지원하지않는 경우도 있다.
+    //console.log(obj.get('startpage'))
+    // 이러한 URLSearchParams 의 단점을 보완하기위해
+    // npm i query-string 모듈을 설치한다
+    // 그럼 위의 두가지 단점을 보완할수 있습니다.
+    // query-string 
+    const query = queryString.parse(searchParams)
+    console.log(query)
+
+
 
     return(
     <div>
         <h1> QnA 게시판입니다.</h1>
+        {query.startpage && <p>시작 페이지는 {query.startpage} 입니다.</p>}
             
             <table border= "1" >
                 <thead>
