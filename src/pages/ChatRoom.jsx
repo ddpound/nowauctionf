52f837e4264d1437735df0f507f6b0ead490f10b
dpound <dpound@naver.com>
2022-05-02 23:15:04 +0900

    채팅방 뼈대 구상중

--- src/pages/ChatRoom.jsx
+++ src/pages/ChatRoom.jsx
@@ -11,6 +11,7 @@ export default function ChatRoom(props) {
         <div class="col-sm">
           <div class="row h-50">
             <div class="col-sm">현재 판매 물품 내용</div>
+            <div class="col-sm">컨트롤러가 있는 div 버튼들 포함</div>
           </div>
           <div class="row h-50">
             <div class="col-sm">채팅창</div>
