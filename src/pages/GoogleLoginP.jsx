9bc5cc143b5f97039efeacf9be9df824922d3223
dpound <dpound@naver.com>
2022-05-21 08:27:03 +0900

    비밀번호 설정 , 따로 숨기기

--- src/pages/GoogleLoginP.jsx
+++ src/pages/GoogleLoginP.jsx
@@ -4,9 +4,11 @@ import axios from "axios";
 
 // 현재 어플리케이션 api 2번 아이디를 따르고있음
 
+const login_key = process.env.REACT_APP_google_login_API_KEY;
+
 export default function GoogleLoginP() {
   return (
-    <GoogleOAuthProvider clientId="1013346603521-o3f3sr148gv12tbahtqplelcaq6u18jk.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId={login_key}>
       <GoogleLogin
         onSuccess={(credentialResponse) => {
           // 현재 암호화된 토큰을 받아온걸 이걸 그대로 전해주면 될듯 백엔드쪽에
