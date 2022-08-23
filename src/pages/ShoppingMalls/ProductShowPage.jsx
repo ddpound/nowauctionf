8bdcb49c17b32de8c7ef9e1af3acb97bc391a0ec
dpound <dpound@naver.com>
2022-08-24 00:04:25 +0900

    제품보기 페이지 완료 ... 이라는 개념 습득완료 바로 setState를 랜더시킬수있게 할수있는듯 나중에 자세히 알아볼예정 이상

--- src/pages/ShoppingMalls/ProductShowPage.jsx
+++ src/pages/ShoppingMalls/ProductShowPage.jsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import { React, useState, useEffect, useRef } from "react";
 import "bootstrap/dist/js/bootstrap.bundle";
 
+import SunEditor from "suneditor-react";
+import { Viewer } from "@toast-ui/react-editor";
 import {
   requestPostHaveToken,
   requestGetHaveToken,
@@ -14,14 +16,31 @@ import {
 export default function ProductShowPage(props) {
   const productId = props.match.params.id;
 
-  const [product, setProduct] = useState({});
+  const [product, setProduct] = useState(...[]);
 
   useEffect(() => {
     axios.get("/show-shoppingmall/product-show/" + productId).then((res) => {
-      console.log(res.data);
       setProduct(res.data);
     });
   }, []);
 
-  return <div className="container">{product.content}</div>;
+  console.log(product);
+  return (
+    <div className="container mt-5">
+      {!!product && (
+        <div className="card mb-3">
+          <div className="card-header bg-transparent ">
+            <h1 style={{ fontWeight: "bold" }}>{product.productName}</h1>
+            <h2>가격 : {product.productPrice}</h2>
+            <h2>수량 : {product.productQuantity}</h2>
+          </div>
+
+          <div className="card-body text-dark">
+            <Viewer initialValue={product.content}></Viewer>
+          </div>
+          <div className="card-footer bg-transparent">{product.createDate}</div>
+        </div>
+      )}
+    </div>
+  );
 }
